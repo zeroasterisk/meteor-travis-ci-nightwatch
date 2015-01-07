374435c1d4e6f4da4c0944c579edd7425b69edca
alan bount <alan@zeroasterisk.com>
2015-01-06 23:33:14 -0500

    basic nightwatch script for Leaderboard
    $ cd app
    $ meteor reset
    $ meteor
    
    $ ./nightwatch --config nightwatch.json --test tests/leaderboard.js
    Starting selenium server... started - PID:  92355
    
    [Leaderboard] Test Suite
    ========================
    
    Running:  Leaderboard Test Basics
    
    ✔  Element <body> was visible after 81 milliseconds.
    ✔  Element <div.message> was visible after 22 milliseconds.
    ✔  Element <ol li.player> was visible after 19 milliseconds.
    ✔  Testing if element <div.details div.name> contains text: "Carl Friedrich Gauss".
    ✔  Testing if element <div.details button.inc> contains text: "Add 5 points".
    ✔  Testing if element <li.selected span.score> contains text: "85".
    ✔  Testing if element <li.selected span.score> contains text: "90".
    ✔  Testing if element <li.selected span.score> contains text: "95".
    ✔  Testing if element <div.details div.name> contains text: "Carl Friedrich Gauss".
    
    OK. 9 total assertions passed. (4.27s)

---
+++ tests/leaderboard.js
@@ -0,0 +1,40 @@
+module.exports = {
+  "Leaderboard Test Basics" : function (browser) {
+    browser
+      .url("http://localhost:3000/")
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('div.message', 5000)
+      .waitForElementVisible('ol li.player', 1000)
+      .click('ol li.player:nth-child(1)')
+      .pause(100)
+      .getText('li.selected span.name', function(result) {
+        this.assert.containsText('div.details div.name', result.value);
+      })
+      .getText('li.selected span.score', function(result) {
+        var selectedScore = parseInt(result.value);
+        this.pause(100)
+        this.assert.containsText('div.details button.inc', 'Add 5 points')
+
+        this.click('div.details button.inc')
+        this.pause(100)
+        // verify the score went up by 5
+        this.assert.containsText('li.selected span.score', selectedScore + 5)
+
+        this.click('div.details button.inc')
+        this.pause(100)
+        // verify the score went up by 5 again
+        this.assert.containsText('li.selected span.score', selectedScore + 10)
+
+        this.click('div.details button.inc')
+        this.pause(100)
+        // verify the score went up by 5 again
+        this.assert.containsText('li.selected span.score', selectedScore + 15)
+        // verify the selected name hasn't changed
+        this.getText('li.selected span.name', function(result) {
+          this.assert.containsText('div.details div.name', result.value);
+        })
+      })
+      .end();
+  }
+};
+
